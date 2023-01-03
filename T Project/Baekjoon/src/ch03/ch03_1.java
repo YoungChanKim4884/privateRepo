e815c0bc757deed294290bb0f8a2db64c2c31826
youngchanKim <kyc4478@hanmail.net>
2023-01-03 20:16:24 +0900

    230103_2000

---
+++ T Project/Baekjoon/src/ch03/ch03_1.java
@@ -0,0 +1,21 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class ch03_1 {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int A = sc.nextInt();
+	    
+	    System.out.println(A + " * 1 = " + A);
+	    System.out.println(A + " * 2 = " + A*2);
+	    System.out.println(A + " * 3 = " + A*3);
+	    System.out.println(A + " * 4 = " + A*4);
+	    System.out.println(A + " * 5 = " + A*5);
+	    System.out.println(A + " * 6 = " + A*6);
+	    System.out.println(A + " * 7 = " + A*7);
+	    System.out.println(A + " * 8 = " + A*8);
+	    System.out.println(A + " * 9 = " + A*9);
+	}
+}
