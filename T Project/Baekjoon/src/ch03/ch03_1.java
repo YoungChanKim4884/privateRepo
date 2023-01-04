b79ec4567ab3d20b89e39a7efc8767406029c50c
youngchanKim <kyc4478@hanmail.net>
2023-01-04 11:33:50 +0900

    230104_1130

--- T Project/Baekjoon/src/ch03/ch03_1.java
+++ T Project/Baekjoon/src/ch03/ch03_1.java
@@ -8,14 +8,9 @@ public class ch03_1 {
 	    
 	    int A = sc.nextInt();
 	    
-	    System.out.println(A + " * 1 = " + A);
-	    System.out.println(A + " * 2 = " + A*2);
-	    System.out.println(A + " * 3 = " + A*3);
-	    System.out.println(A + " * 4 = " + A*4);
-	    System.out.println(A + " * 5 = " + A*5);
-	    System.out.println(A + " * 6 = " + A*6);
-	    System.out.println(A + " * 7 = " + A*7);
-	    System.out.println(A + " * 8 = " + A*8);
-	    System.out.println(A + " * 9 = " + A*9);
+	    for(int i=1; i<=9; i++) {
+	    	System.out.println(A + " * " + i + " = " + A*i);
+	    }
+
 	}
 }
