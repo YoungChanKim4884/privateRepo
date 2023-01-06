433fcfcc7b9c2d87bce6f50125f9c3a7d8835a36
youngchanKim <kyc4478@hanmail.net>
2023-01-06 10:19:36 +0900

    230106_1000

---
+++ T Project/Prac/src/programmers/ch00/ch00_1.java
@@ -0,0 +1,9 @@
+package programmers.ch00;
+
+public class ch00_1 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 % num2;
+        return answer;
+    }
+}
