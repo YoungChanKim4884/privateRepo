fe8c438192d5a31f3b1e82331ccbda1515040e29
youngchanKim <kyc4478@hanmail.net>
2023-02-09 23:45:33 +0900

    230209_2330

--- T Project/Prac/src/test.java
+++ T Project/Prac/src/test.java
@@ -1,10 +1,5 @@
-import java.util.Arrays;
-
 public class test {
-	public static void main(String[] args) {
-		int[] answer = {};
-		answer[0] = 5;
-		answer[1] = 6;
-		System.out.println(answer[0]);
-	}
+    public static void main(String[] args) {
+    	
+    }
 }
