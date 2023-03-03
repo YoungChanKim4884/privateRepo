0bfc67ea91874886b612880647d9fde7bc4546e6
youngchanKim <kyc4478@hanmail.net>
2023-03-03 23:06:34 +0900

    230303_2300

---
+++ Study/Prac/src/baekjoon/ch04/ch04_8_3052.java
@@ -0,0 +1,26 @@
+package baekjoon.ch04;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ch04_8_3052 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0; i<10; i++){
+            int num = sc.nextInt() % 42;
+            int cnt = 0;
+            for(int j=0; j<arr.size(); j++){
+                if(arr.get(j).equals(num)){
+                    cnt++;
+                }
+            }
+            if(cnt==0){
+                arr.add(num);
+            }else{
+                cnt = 0;
+            }
+        }
+        System.out.print(arr.size());
+    }
+}
