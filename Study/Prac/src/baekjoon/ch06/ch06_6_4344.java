7d4a591006df942b9485cf5012ec926d0e74b677
youngchanKim <kyc4478@hanmail.net>
2023-03-05 13:53:02 +0900

    230305_1400

---
+++ Study/Prac/src/baekjoon/ch06/ch06_6_4344.java
@@ -0,0 +1,43 @@
+package baekjoon.ch06;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class ch06_6_4344 {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int C = Integer.parseInt(br.readLine());
+        int N, cnt = 0;
+        double sum = 0, avg, result;
+        
+        for(int i=1; i<=C; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	N = Integer.parseInt(st.nextToken());
+        	int[] score = new int[N];
+        	for(int j=0; j<N; j++) {
+        		score[j] = Integer.parseInt(st.nextToken());
+        		sum += score[j];
+        	}
+        	avg = (double)sum / N;
+        	for(int j=0; j<N; j++) {
+        		if(avg<score[j]) {
+        			cnt++;
+        		}
+        	}
+        	result = (double)cnt / N * 100;
+        	bw.write(String.format("%.3f", result) + "%");
+        	bw.newLine();
+        	cnt = 0;
+        	sum = 0; 
+        }
+        bw.flush();
+	}
+}
---
+++ Study/Prac/src/baekjoon/ch06/ch06_7_2941.java
@@ -0,0 +1,20 @@
+package baekjoon.ch06;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class ch06_7_2941 {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        
+        str = str.replace("c=", "1").replace("c-", "2").replace("dz=", "3").replace("d-", "4").replace("lj", "5").replace("nj", "6").replace("s=", "7").replace("z=", "8");
+        bw.write("" + str.length());
+        bw.flush();
+	}
+}
